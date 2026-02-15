816c62426506feb0fdef655799abefd289f1ccb3
Viktoriya3107 <bondarenko_vikyoriya988@ukr.net>
2026-02-15 12:10:29 +0200

    hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "ТУТ_СВІЙ_API_KEY";
+
+export function getImagesByQuery(query) {
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+    },
+  })
+  .then(response => response.data);
+}
