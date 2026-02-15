fccf744f92bc9ece505a699233e96915520ed79c
Viktoriya3107 <bondarenko_viktoriya988@ukr.net>
2026-02-15 12:11:25 +0000

    Deploying to gh-pages from @ Viktoriya3107/goit-js-hw-11@0dcd45b4d77888eea169acb85c62ac6ba9ce5d10 🚀

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as d,S as f}from"./assets/vendor-Cv5692A3.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const p="https://pixabay.com/api/",m="54663882-ec3b2d69ddbcbb3faf32c451a";function y(n,o=1,t=12){return d.get(p,{params:{key:m,q:n,image_type:"photo",orientation:"horizontal",safesearch:!0,page:o,per_page:t}}).then(a=>a.data.hits)}const l=document.querySelector(".gallery"),u=document.querySelector(".loader"),h=new f(".gallery a",{captionsData:"alt",captionDelay:250});function g(n){const o=n.map(t=>`
+    <li class="gallery-item">
+      <a href="${t.largeImageURL}">
+        <img src="${t.webformatURL}" alt="${t.tags}" />
+      </a>
+      <div class="info">
+        <p>Likes: ${t.likes}</p>
+        <p>Views: ${t.views}</p>
+        <p>Comments: ${t.comments}</p>
+        <p>Downloads: ${t.downloads}</p>
+      </div>
+    </li>
+  `).join("");l.insertAdjacentHTML("beforeend",o),h.refresh()}function L(){l.innerHTML=""}function b(){u.classList.add("active")}function v(){u.classList.remove("active")}const i=document.querySelector(".form");let c=1;i.addEventListener("submit",async n=>{n.preventDefault();const t=i.querySelector("input[name='search-text']").value.trim();if(t){c=1,L(),b();try{const a=await y(t,c);g(a)}catch(a){console.error("Помилка запиту:",a)}finally{v()}}});
 //# sourceMappingURL=index.js.map
