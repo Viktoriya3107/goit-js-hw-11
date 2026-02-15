50910e9fcdb193ced509b3c747f2f69e567ba9e9
Viktoriya3107 <bondarenko_viktoriya988@ukr.net>
2026-02-15 12:26:42 +0000

    Deploying to gh-pages from @ Viktoriya3107/goit-js-hw-11@2ea47033d5bdd52830ec3db10d11ba79aa0d88a5 🚀

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import{a as d,S as f}from"./assets/vendor-Cv5692A3.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const p="https://pixabay.com/api/",m="54663882-ec3b2d69ddbcbb3faf32c451a";function y(n,o=1,t=12){return d.get(p,{params:{key:m,q:n,image_type:"photo",orientation:"horizontal",safesearch:!0,page:o,per_page:t}}).then(a=>a.data.hits)}const l=document.querySelector(".gallery"),u=document.querySelector(".loader"),h=new f(".gallery a",{captionsData:"alt",captionDelay:250});function g(n){const o=n.map(t=>`
+import{a as p,S as d}from"./assets/vendor-Cv5692A3.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();const f="https://pixabay.com/api/",m="54663882-ec3b2d69ddbcbb3faf32c451a";function y(s,i=1,e=12){return p.get(f,{params:{key:m,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,page:i,per_page:e}}).then(o=>o.data.hits)}const l=document.querySelector(".gallery"),u=document.querySelector(".loader"),g=new d(".gallery a",{captionsData:"alt",captionDelay:250});function h(s){const i=s.map(e=>`
     <li class="gallery-item">
-      <a href="${t.largeImageURL}">
-        <img src="${t.webformatURL}" alt="${t.tags}" />
+      <a href="${e.largeImageURL}">
+        <img src="${e.webformatURL}" alt="${e.tags}" />
       </a>
       <div class="info">
-        <p>Likes: ${t.likes}</p>
-        <p>Views: ${t.views}</p>
-        <p>Comments: ${t.comments}</p>
-        <p>Downloads: ${t.downloads}</p>
+        <p>Likes: ${e.likes}</p>
+        <p>Views: ${e.views}</p>
+        <p>Comments: ${e.comments}</p>
+        <p>Downloads: ${e.downloads}</p>
       </div>
     </li>
-  `).join("");l.insertAdjacentHTML("beforeend",o),h.refresh()}function L(){l.innerHTML=""}function b(){u.classList.add("active")}function v(){u.classList.remove("active")}const i=document.querySelector(".form");let c=1;i.addEventListener("submit",async n=>{n.preventDefault();const t=i.querySelector("input[name='search-text']").value.trim();if(t){c=1,L(),b();try{const a=await y(t,c);g(a)}catch(a){console.error("Помилка запиту:",a)}finally{v()}}});
+  `).join("");l.insertAdjacentHTML("beforeend",i),g.refresh()}function L(){l.innerHTML=""}function b(){u.classList.add("active")}function v(){u.classList.remove("active")}const n=document.querySelector(".form");let c=1;n.addEventListener("submit",async s=>{s.preventDefault();const e=n.querySelector("input[name='search-text']").value.trim();if(!e){iziToast.warning({title:"Увага",message:"Будь ласка, введіть запит для пошуку.",position:"topRight"});return}c=1,L(),b();try{const o=await y(e,c);if(!o||o.length===0){iziToast.error({title:"Помилка",message:`За запитом "${e}" зображень не знайдено.`,position:"topRight"});return}h(o)}catch(o){console.error("Помилка запиту:",o),iziToast.error({title:"Помилка",message:"Сталася помилка під час завантаження зображень. Спробуйте пізніше.",position:"topRight"})}finally{v()}});
 //# sourceMappingURL=index.js.map
